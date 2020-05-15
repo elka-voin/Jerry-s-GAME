3d1f0592c7860732b325bc545786422f6ff4b9b0
elka-voin <63861686+elka-voin@users.noreply.github.com>
2020-05-15 18:43:17 +0300

    add Summer and Beth

--- Main.py
+++ Main.py
@@ -1,11 +1,40 @@
-import pygame, class_balloon
+import pygame, class_balloon, random, time
 pygame.init()
 
-#nastroyki эkrana#\
-w = 600
-h = 750
+def start_menu():
+    gamedisplay.blit(fone3,(0,0))
+    play = pygame.image.load("PLAY.png")
+    play = pygame.transform.scale(play,(400,400))
+    gamedisplay.blit (play,(100,100))
+    pygame.display.update()
+    #time.sleep(10)
+    maus_button = False
+    while maus_button == False:
+        for e in pygame.event.get():
+            if e.type==pygame.MOUSEBUTTONDOWN:
+                maus_button = True
+
+#nastroyki эkrana
+w = 1000
+h = 800
 gamedisplay = pygame.display.set_mode((w,h))
 pygame.display.set_caption("Jarry's Game")
+
+#фоны
+fone1 = pygame.image.load("DISPLAY.jpg")
+fone1 = pygame.transform.scale(fone1,(w,h))
+
+fone2 = pygame.image.load("class.jpg")
+fone2 = pygame.transform.scale(fone2,(w,h))
+
+fone3 = pygame.image.load("rick.jpg")
+fone3 = pygame.transform.scale(fone3,(w,h))
+#fone3 = fone3.subsurface((200,0,1000,675))
+
+#второстепенный фон
+surface = pygame.Surface((w,h))
+surface.fill((0,0,0))
+surface.set_alpha(130)
 
 #FPS
 clock = pygame.time.Clock()
@@ -21,50 +50,124 @@ spawn = pygame.event.Event(pygame.USEREVENT)
 spawn_time = start_ticks
 cooll_douwn = 1000
 
+beth = False
+jarry = pygame.sprite.Group()
+summer = class_balloon.Summer()
+kick = 0
+fone = fone1
 false = 0
-
 balloons = pygame.sprite.LayeredUpdates()
-
+start_menu()
 game = True
-while game:   
-    clock.tick(FPS)
-    gamedisplay.fill((30, 80, 227, 0.59))  
-    balloons.draw(gamedisplay)
-    balloons.update()
-
+while game: 
+    
+    #ticks
+    clock.tick(FPS)  
     now_ticks = pygame.time.get_ticks()
-    #time = now_ticks - start_ticks
-    #print (time//1000)
     cooll_douwn = 1000 - false * 50 
     if now_ticks - spawn_time > cooll_douwn:
         pygame.event.post(spawn)
-        
-
+  
+    #обработка событий
     events = pygame.event.get()
     for e in events:
 
         if e.type==pygame.QUIT:
             game = False
 
+        if e.type==pygame.KEYDOWN:
+
+            if e.key==pygame.K_1:
+                fone = fone1
+
+            if e.key==pygame.K_2:
+                fone = fone2
+
+            if e.key==pygame.K_3:
+                fone = fone3
+        # генерация шариков
         if e.type==pygame.USEREVENT:
-            b = class_balloon.Balloon()
+            r = random.randint(1,15)
+            if r == 7:
+                b  = class_balloon.Head()
+            elif r == 8:
+                b = class_balloon.Jerry()
+                jarry.add(b) 
+                if len(jarry.sprites()) > 5 and beth == False:
+                    b = class_balloon.Beth()
+                    beth = True
+                    print (7)
+                else:
+                    b = class_balloon.Jerry()
+                    jarry.add(b)
+
+            elif r == 9:
+                b = class_balloon.Summer()
+
+            else:
+
+                if false > 5:
+                    image = "rick"
+
+                elif kick > 14:
+                    image = "morty"
+                else:
+                    image = "balloon"
+
+                b = class_balloon.Balloon(image)
             balloons.add(b)
             spawn_time = now_ticks
 
+        # убиватель шариков
         if e.type == pygame.MOUSEBUTTONDOWN:
             if e.button==1:
                 
                 click_balloon = balloons.get_sprites_at(e.pos)
                 for b in click_balloon:
                     b.kill()
+                    if type (b) != class_balloon.Jerry:
+                        kick += b.cick
+                        if type (b) == class_balloon.Summer:
+                            for e in balloons:
+                                if type (e) == class_balloon.Balloon:
+                                    if random.randint (1,2) == 2:
+                                        e.kill()
+                             
+
+                            if false < 150:
+                                false = 0
+
+                            else: 
+                                false -= 150
+                        elif type (b) == class_balloon.Beth:
+                            beth = False
+                            for g in jarry:
+                                g.kill()
+                    else:
+                        false += 5
+                    
+
 
     for b in balloons:
         if b.check_pos() == False:
             b.kill()
             false += 1
+    #вывод фона
+    gamedisplay.blit(fone,(0,0))
+    gamedisplay.blit(surface,(0,0))
 
+    #отрисовка шаров
+    balloons.draw(gamedisplay)
+    balloons.update()
+
+    #вывод текста
     text = f.render("false: " + str(false),1,(100,250,220))
     gamedisplay.blit(text,(0,0))
+
+    text2 = f.render("kills: " + str(kick),1,(100,250,220))
+    text2_rect  = text2.get_rect()
+    text2_rect.right = w
+    gamedisplay.blit(text2,text2_rect)
 
     pygame.display.update()
 
--- class_balloon.py
+++ class_balloon.py
@@ -10,19 +10,71 @@ for i in range (3):
     image = images[i]
     image = pygame.transform.scale(image,(160,200))
     images [i] = image 
+morty = pygame.image.load("morty.png")
+sanches = pygame.image.load("RICK.png")
+#sanches.convert_alpha()    
 
 class Balloon(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self,image = "balloon"):
         pygame.sprite.Sprite.__init__(self)
-        self.image = choice(images)
+        if image =="balloon":            
+            self.image = choice(images)
+        elif image == "morty":
+            self.image = pygame.transform.scale(morty,(160,200))
+        elif image == "rick":
+            self.image = pygame.transform.scale(sanches,(160,200))
         self.rect = self.image.get_rect()
         self.speed  = 10
         self.rect.y = 750
-        self.rect.x = randint (10,390)
+        self.rect.x = randint (10,790)
+        self.sanches_mode = False
+        self.cick = 1
     def update(self):
+
         self.rect.y -= self.speed
     def check_pos (self):   
         if self.rect.bottom < 0:
             return False
         else:
             return True    
+
+class Head(Balloon):
+    def __init__(self):
+        Balloon.__init__(self)
+        self.image = pygame.image.load ("HEAD.png") 
+        self.image = pygame.transform.scale(self.image,(190,220))
+        self.rect = self.image.get_rect()
+        self.speed  = 25
+        self.rect.y = 750
+        self.rect.x = randint (10,790)       
+        self.cick = 666
+
+class Jerry(Balloon):
+    def __init__(self):
+        Balloon.__init__(self)
+        self.image = pygame.image.load ("Jerry.png") 
+        self.image = pygame.transform.scale(self.image,(170,220))
+        self.rect = self.image.get_rect()
+        self.speed  = 1
+        self.rect.y = 750
+        self.rect.x = randint (10,790)       
+
+class Summer(Balloon):
+    def __init__(self):
+        Balloon.__init__(self)
+        self.image = pygame.image.load ("Summer.png") 
+        self.image = pygame.transform.scale(self.image,(170,220))
+        self.rect = self.image.get_rect()
+        self.speed  = 9
+        self.rect.y = 750
+        self.rect.x = randint (10,790)
+
+class Beth(Balloon):
+    def __init__(self):
+        Balloon.__init__(self)
+        self.image = pygame.image.load ("Beth.png") 
+        self.image = pygame.transform.scale(self.image,(170,220))
+        self.rect = self.image.get_rect()
+        self.speed  = 7
+        self.rect.y = 750
+        self.rect.x = randint (10,790)
